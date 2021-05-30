6b551739526866c7f35057caf1bb5811512430bf
Ingrid Lyublinsky <ingrid.lyub@gmail.com>
2021-05-30 12:51:47 -0700

    started working through routes and installed a few dependecies

--- Develop/routes/apiRoute.js
+++ Develop/routes/apiRoute.js
@@ -1,26 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+
 // LOAD DATA
 
-const noteDB = require('../db/db');
+const noteDB = require('../db/db.json');
 
 // ROUTING
 
 module.exports = (app) => {
   // API GET Requests
 
-  app.get('/api/htmlRoute', (req, res) => res.json(noteDB));
+  app.get('/api/notes', (req, res) => res.json(noteDB));
 
   // API POST Requests
 
-  app.post('/api/', (req, res) => {
+  app.post('/api/notes', (req, res) => {
+    let newNote = req.body;
+    newNote.id = uuidv4();
+    data.push(newNote)
+    fs.writeFileSync('./db/db.json', JSON.stringify(noteDB))
+    res.json(noteDB)
+    console.log("Note added successfully!")
+    });
 
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    } else {
-      waitListData.push(req.body);
-      res.json(false);
+  // API DELETE Requests
+
+  app.delete('/api/notes/:id', function (req, res) {
+    let id = req.params.id
+    const select = noteDB.find(note => note.id === id)
+
+    if (id === select.id){
+      data.splice(data.indexOf(select),1)
+      fs.writeFileSync('./db/db.json', JSON.stringify(noteDB))
     }
-  });
+    res.json(noteDB)
+    console.log("Note successfully deleted!")
+  })
+}
 
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
@@ -28,7 +46,7 @@ module.exports = (app) => {
   app.post('/api/clear', (req, res) => {
     // Empty out the arrays of data
     tableData.length = 0;
-    waitListData.length = 0;
+    waitLisnpmtData.length = 0;
 
     res.json({ ok: true });
   });
--- Develop/routes/htmlRoute.js
+++ Develop/routes/htmlRoute.js
@@ -4,17 +4,11 @@ const path = require('path');
 
 module.exports = (app) => {
   // => HTML GET Requests
+    app.get("/notes", (req, res) => {
+        res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
+    });
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
-
-  app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-  });
-
-  // Default route, if no match.
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
-};
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
+    });
+}
